6b4c965e44d7422945d15aa58392485c8f2f7457
Peter Max Dreyer <petrusd987@gmail.com>
2013-03-12 14:27:01 -0700

    Began implementing auto configuration.

---
+++ src/me/petrusd987/LoudMouth/Config.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package me.petrusd987.LoudMouth;
+
+/**
+ *
+ * @author Petrusd987
+ */
+public class Config {
+    public static String CONFIG_FOLDER = "./LoudMouth/";
+}
--- src/me/petrusd987/LoudMouth/LoudMouth.java
+++ src/me/petrusd987/LoudMouth/LoudMouth.java
@@ -1,5 +1,9 @@
 package me.petrusd987.LoudMouth;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class LoudMouth extends JavaPlugin{
@@ -15,5 +19,26 @@ public class LoudMouth extends JavaPlugin{
     
     public static void PluginInfo(String message){
         System.out.println("[LoudMouth] " + message);
+    }
+    
+    
+    /* The following function checks to ensure that the configuration
+     * files and the folder in which they are contained exists*/
+    
+    public boolean checkFilesExists(){
+        boolean fileNotFound = false;
+        File folderFile = new File(Config.CONFIG_FOLDER);
+        
+        //Can't have files without a folder.
+        if(!folderFile.exists()){
+            fileNotFound = true;
+            PluginInfo("Configuration folder not found...Creating");
+            try {
+                folderFile.createNewFile();
+            } catch (IOException ex) {
+               PluginInfo("Could not create config folder!");
+            }
+        }
+        return fileNotFound;
     }
 }
